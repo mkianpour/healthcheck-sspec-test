eaedb2d0f309c1a266ceb7897eadcac5b76bfd8d
Mehdi Kianpour <mehdi.kianpour@solvinity.com>
2016-07-15 16:45:41 +0200

    transfer test project

---
+++ cookbooks/serverspec-healthcheck/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: serverspec-healthcheck
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'sspec-install'
